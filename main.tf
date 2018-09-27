2220b65950e2eb81d79c5b6f50b6f476f89a9264
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:21:12 +0200

    Add documentation header to main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
+/**
+ * [![Build Status](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-lb/job/master/badge/icon)](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-lb/job/master/)
+ * # azurerm lb
+ * The module creates Load Balancers on AzureRM
+ */
+
 provider "azurerm" {}
