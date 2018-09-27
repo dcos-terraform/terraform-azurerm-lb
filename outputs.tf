35915598f5a6695e260019a43e82279fe326185e
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-27 14:20:36 -0700

    terraform fmt after description add

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 # LB Address
 output "elb_address" {
   description = "elb address"
+
   value = "${element(concat(azurerm_public_ip.master_load_balancer_public_ip.*.fqdn,
                             azurerm_public_ip.public_agent_load_balancer_public_ip.*.fqdn,
                             list("")), 0)}"
@@ -9,6 +10,7 @@ output "elb_address" {
 # Public backend address pool ID
 output "public_backend_address_pool" {
   description = "public backend address pool"
+
   value = "${element(concat(azurerm_lb_backend_address_pool.public_master_backend_pool.*.id,
                             azurerm_lb_backend_address_pool.external_public_agent_backend_pool.*.id,
                             list("")), 0)}"
@@ -17,6 +19,6 @@ output "public_backend_address_pool" {
 # Private backend address pool ID
 output "private_backend_address_pool" {
   description = "private backend address pool"
-  value = "${element(concat(azurerm_lb_backend_address_pool.private_master_backend_pool.*.id,
+  value       = "${element(concat(azurerm_lb_backend_address_pool.private_master_backend_pool.*.id,
                             list("")), 0)}"
 }
