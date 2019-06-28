e8f100303183710ca08603365f89ed4c3a6e8873
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-28 16:51:44 +0200

    num of instances added, corrected warnings

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "lb_address" {
   description = "lb address"
-  value       = "${var.internal ? azurerm_lb.load_balancer.private_ip_address : element(concat(azurerm_public_ip.public_ip.*.fqdn,list("")),0)}"
+  value       = "${var.internal ? element(concat(azurerm_lb.load_balancer.*.private_ip_address,list("")),0) : element(concat(azurerm_public_ip.public_ip.*.fqdn,list("")),0)}"
 }
 
 output "backend_address_pool" {
   description = "backend address pool"
-  value       = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+  value       = "${element(concat(azurerm_lb_backend_address_pool.backend_pool.*.id,list("")),0)}"
 }
